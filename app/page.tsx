e557b15bcba6cccacc7423f5dbeda807944a685a
ShukurovOzodbek <maximus25june@gmail.com>
2025-11-30 20:03:39 +0500

    job done now baby

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+    const router = useRouter();
+    useEffect(() => {
+        router.push("/admin");
+    }, [router]);
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
